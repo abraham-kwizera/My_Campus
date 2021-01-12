422f4c069d46a3507f5de5f564db00654c0c9f77
Abraham Kwizera <kwiz.abraham4@gmail.com>
2021-01-12 23:48:10 +0200

    Initial Commit

--- MyCampus/app/src/main/java/com/example/mycampus/SignupActivity.java
+++ MyCampus/app/src/main/java/com/example/mycampus/SignupActivity.java
@@ -5,16 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignupActivity extends AppCompatActivity {
-   TextView sign_in;
+   //variable declaration
+
+    TextView Sign_In;
+    private EditText inputFullName, inputEmail, inputRegNo, inputConfirmRegNo, inputPassword, inputConfirmPassword;
+    private Button buttonSignUp;
+    private ProgressBar progressBar;
+    private FirebaseAuth
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        sign_in = (TextView) findViewById(R.id.SignIn);
-        sign_in.setOnClickListener(new View.OnClickListener() {
+
+        //Getting Firebase Authentication instance
+
+
+
+
+
+        Sign_In = (TextView) findViewById(R.id.SignIn);
+        Sign_In.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
