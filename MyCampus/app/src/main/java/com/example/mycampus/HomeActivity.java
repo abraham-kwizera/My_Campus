2abc14b548c368b365d4221cee59060c4d865710
Abraham Kwizera <kwiz.abraham4@gmail.com>
2021-01-27 19:00:23 +0200

    Initial Commit!

--- MyCampus/app/src/main/java/com/example/mycampus/HomeActivity.java
+++ MyCampus/app/src/main/java/com/example/mycampus/HomeActivity.java
@@ -2,18 +2,33 @@ package com.example.mycampus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 public class HomeActivity extends AppCompatActivity {
 
+    TextView Marks;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Marks = (TextView) findViewById(R.id.marks);
+        Marks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (HomeActivity.this, MarksActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
 
         FloatingActionButton help = findViewById(R.id.help);
         help.setOnClickListener(new View.OnClickListener() {
